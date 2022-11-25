9efcbb69c14f827b74dbb1100da6ae5809207ade
opert0816 <xxbat1001@gmail.com>
2022-11-25 10:01:50 +0800

    1125

---
+++ 1125/app/src/main/java/com/example/a1125/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a1125;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.buttom);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+                Toast.makeText(MainActivity.this,"啟動Service",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
